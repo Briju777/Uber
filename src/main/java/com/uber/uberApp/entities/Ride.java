40c19a2fdf33df7fb54662b99182490b1a53fcea
briju777 <yA040465>
2025-05-04 10:49:13 +0530

    Added RideStatus

--- src/main/java/com/uber/uberApp/entities/Ride.java
+++ src/main/java/com/uber/uberApp/entities/Ride.java
@@ -2,12 +2,16 @@ package com.uber.uberApp.entities;
 
 import com.uber.uberApp.entities.enums.RideStatus;
 import jakarta.persistence.*;
+import org.locationtech.jts.geom.Point;
 
 @Entity
 public class Ride {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+
+    @Column(columnDefinition = "Geometry(Point, 4326")
+    private Point pickupLocation;
 
     @Enumerated(EnumType.STRING)
     private RideStatus rideStatus;
